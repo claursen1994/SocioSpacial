36b2e7b21bf1aacb3faef0ebd826b1d35d0d3e5b
Calvin Laursen <claursen@bournemouth.ac.uk>
2019-10-22 15:28:49 +0100

    Spring Clean 3.0

--- LabCodeC/functions/genDesign.m
+++ LabCodeC/functions/genDesign.m
@@ -14,25 +14,18 @@ else % real subject
    cps = design_same(:,[1, const.ID+1]); % +1 because the first column is item number(%cps is conditions per subject) 
 end
 
-sf = cps(find(mod(cps(:,end),2)==1),:); %b1 is silent
-bf = cps(find(mod(cps(:,end),2)==0),:); %b2 is aloud
+% sf = cps(find(mod(cps(:,end),2)==1),:); %b1 is silent
+% bf = cps(find(mod(cps(:,end),2)==0),:); %b2 is aloud
 
 %Randomise the items within the blocks:
-sfr= sf(randperm(length(sf)),:);
-bfr= bf(randperm(length(bf)),:);
+cpsr= cps(randperm(length(cps)),:);
 
-% Make sure the blocks are counterbalanced and combine the 2 blocks to get a 100 item matrix:
-if mod(const.ID, 2)==1;
-   exp_items = [sfr;bfr]; % small font to big font 
-else % even participants
-   exp_items = [bfr;sfr]; % big font to small font
-end
-
+exp_items= cpsr;
 
 %Practice items:
 % condition always the same (e.g. 9)
-c1 = (41:42)'; % column 1 is item number
-c2 = [1,1]'; %repmat(9,6,1); % column 2 is condition (always 9)
+c1 = (25:26)'; % column 1 is item number
+c2 = [9,9]'; %repmat(9,6,1); % column 2 is condition (always 9)
 pr = horzcat(c1,c2); % 2- column matrix 
 
 %Randomise practice items as above:
@@ -40,7 +33,7 @@ prr = pr(randperm (length(pr)),:);
 
 % % combine practice & experimental items:
 %design = [prr; exp_items];
-design = [exp_items];
+design = [prr; exp_items];
 
 % save design matrix for our records:
 savefile= ['design/sub_matrix/s_' num2str(const.ID) '.mat'];
