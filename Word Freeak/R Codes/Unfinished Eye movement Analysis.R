42b4a94dc85c197b5e11f853a957874ec217a432
Calvin Laursen <claursen@bournemouth.ac.uk>
2020-01-01 20:50:27 +0000

    added

--- Word Freeak/R Codes/Unfinished Eye movement Analysis.R
+++ Word Freeak/R Codes/Unfinished Eye movement Analysis.R
@@ -3,9 +3,11 @@
 ################
 # Trial times: #
 ################
+library(EMreading)
+
 rm(list= ls())
 data_dir= ("H:/Profile/Desktop/worb 2/SocioSpacial/SoSpaPilotASC")
-
+data_dir=("C:/Users/Calvin/Desktop/Proc/List Files")
 if(!file.exists("data/Trial_time.Rda")){
   
   Trialt<- trialTime(data_list = data_dir, maxtrial = 100)
